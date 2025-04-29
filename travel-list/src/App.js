07b5b39277a8fd33bf5c1d9f3fd2420a330f87fd
tomshaffery <98631104+tomshaffery@users.noreply.github.com>
2025-04-29 18:24:37 -0400

    Items and packed condition rendered

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -1,3 +1,8 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+];
+
 export default function App() {
   return (
     <div className="app">
@@ -10,7 +15,7 @@ export default function App() {
 }
 
 function Logo() {
-  return <h3>🌴 Far Away 🧳</h3>;
+  return <h1>🌴 Far Away 🧳</h1>;
 }
 
 function Form() {
@@ -22,12 +27,31 @@ function Form() {
 }
 
 function PackingList() {
-  return <div className="list">List</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
 }
 
 function Stats() {
   return (
-    <footer>
+    <footer className="stats">
       <em>
         🧳 You have X items on your list and you already packed X% of the total.
       </em>
