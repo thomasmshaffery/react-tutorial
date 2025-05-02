09e69081bf8eac9e6ff104d0ed959b691a344b79
tomshaffery <98631104+tomshaffery@users.noreply.github.com>
2025-05-02 07:36:44 -0400

    Clear list function

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -19,6 +19,13 @@ export default function App() {
     );
   }
 
+  function handleClearItems() {
+    const confirmed = window.confirm(
+      "Are you sure you want to delete all items?"
+    );
+    if (confirmed) setItems([]);
+  }
+
   return (
     <div className="app">
       <Logo />
@@ -27,6 +34,7 @@ export default function App() {
         items={items}
         onDeleteItem={handleDeleteItem}
         onToggleItem={handleToggleItem}
+        onClearItems={handleClearItems}
       />
       <Stats items={items} />
     </div>
@@ -79,7 +87,7 @@ function Form({ onAddItems }) {
   );
 }
 
-function PackingList({ items, onDeleteItem, onToggleItem }) {
+function PackingList({ items, onDeleteItem, onToggleItem, onClearItems }) {
   const [sortBy, setSortBy] = useState("input");
   let sortedItems;
 
@@ -101,6 +109,7 @@ function PackingList({ items, onDeleteItem, onToggleItem }) {
             item={item}
             onDeleteItem={onDeleteItem}
             onToggleItem={onToggleItem}
+            onClearItems={onClearItems}
             key={item.id}
           />
         ))}
@@ -112,6 +121,7 @@ function PackingList({ items, onDeleteItem, onToggleItem }) {
           <option value="description">Sort by description</option>
           <option value="packed">Sort by packed status</option>
         </select>
+        <button onClick={onClearItems}>Clear list</button>
       </div>
     </div>
   );
