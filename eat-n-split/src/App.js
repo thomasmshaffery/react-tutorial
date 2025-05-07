f6e50e7070a4a3a9eb1f7885871c3491f64c391b
tomshaffery <98631104+tomshaffery@users.noreply.github.com>
2025-05-07 01:15:40 -0400

    Buttons added

--- eat-n-split/src/App.js
+++ eat-n-split/src/App.js
@@ -47,7 +47,7 @@ function Friend({ friend }) {
       <h3>{friend.name}</h3>
       {friend.balance < 0 && (
         <p className="red">
-          You owe {friend.name} {Math.abs(friend.balance)}
+          You owe {friend.name} {Math.abs(friend.balance)}€
         </p>
       )}
       {friend.balance === 0 && (
@@ -55,9 +55,10 @@ function Friend({ friend }) {
       )}
       {friend.balance > 0 && (
         <p className="green">
-          {friend.name} owes you {Math.abs(friend.balance)}
+          {friend.name} owes you {Math.abs(friend.balance)}€
         </p>
       )}
+      <button className="button">Select</button>
     </li>
   );
 }
