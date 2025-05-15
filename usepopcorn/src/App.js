7e3365126ba031372709241149fef30e989a9d08
tomshaffery <98631104+thomasmshaffery@users.noreply.github.com>
2025-05-15 16:22:26 -0400

    Title cleanup function

--- usepopcorn/src/App.js
+++ usepopcorn/src/App.js
@@ -191,6 +191,10 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
     function () {
       if (!title) return;
       document.title = `Movie | ${title}`;
+
+      return function () {
+        document.title = "usePopcorn";
+      };
     },
     [title]
   );
