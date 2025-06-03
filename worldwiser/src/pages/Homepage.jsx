72a33c37afb75433d5277ff753e75ee21cd3192f
tomshaffery <98631104+thomasmshaffery@users.noreply.github.com>
2025-06-03 18:22:19 -0400

    Pages added, navlinks added

---
+++ worldwiser/src/pages/Homepage.jsx
@@ -0,0 +1,12 @@
+import PageNav from "../components/PageNav";
+
+function Homepage() {
+  return (
+    <div>
+      <PageNav />
+      <h1>WorldWise</h1>
+    </div>
+  );
+}
+
+export default Homepage;
