39b65feb89167ad07d0b28a4835ace645ffe276b
tomshaffery <98631104+thomasmshaffery@users.noreply.github.com>
2025-07-29 16:23:07 -0400

    adding query strings and params

--- worldwiser/src/components/City.jsx
+++ worldwiser/src/components/City.jsx
@@ -1,9 +1,11 @@
-import { useParams } from "react-router-dom";
+import { useParams, useSearchParams } from "react-router-dom";
 import styles from "./City.module.css";
 
 function City() {
   const { id } = useParams();
-
+  const [searchParams, setSearchParams] = useSearchParams();
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
   // TEMP DATA
   const currentCity = {
     cityName: "Lisbon",
@@ -14,7 +16,14 @@ function City() {
 
   const { cityName, emoji, date, notes } = currentCity;
 
-  return <h1>City {id}</h1>;
+  return (
+    <>
+      <h1>City {id}</h1>
+      <p>
+        Position: {lng}, {lat}
+      </p>
+    </>
+  );
 
   // return (
   //   <div className={styles.city}>
