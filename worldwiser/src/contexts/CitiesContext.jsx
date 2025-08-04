298d08e0ebca7872d57d45be22dc4650d38b391b
tomshaffery <98631104+thomasmshaffery@users.noreply.github.com>
2025-08-04 16:09:15 -0400

    refactored wordlwiser with context

---
+++ worldwiser/src/contexts/CitiesContext.jsx
@@ -0,0 +1,46 @@
+import { createContext, useContext, useEffect, useState } from "react";
+
+const CitiesContext = createContext();
+
+function CitiesProvider({ children }) {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch("./cities.json");
+        if (!res.ok) throw new Error("Unable to retrieve citiess.");
+
+        const data = await res.json();
+        setCities(data.cities);
+      } catch {
+        alert("There was an error loading data...");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+
+  return (
+    <CitiesContext.Provider
+      value={{
+        cities,
+        isLoading,
+      }}
+    >
+      {children}
+    </CitiesContext.Provider>
+  );
+}
+
+function useCities() {
+  const context = useContext(CitiesContext);
+  if (context === undefined)
+    throw new Error("CitiesContext was used outside the CitiesProvider.");
+  return context;
+}
+
+export { CitiesProvider, useCities };
